7fbc3d75efc07520d24929646c4308a384a32bf4
Scogun <santonov@ucasoft.com>
2018-02-22 20:42:17 +0100

    Add currencies

--- app/src/main/java/com/ucasoft/money/model/MoneyCurrency.kt
+++ app/src/main/java/com/ucasoft/money/model/MoneyCurrency.kt
@@ -4,25 +4,38 @@ import java.util.*
 
 class MoneyCurrency {
 
-    var amount: Double = 0.0
+    var balance: Double = 0.0
 
     lateinit var currencyCode: String
 
     override fun toString(): String {
 
         if (Symbols.containsKey(currencyCode)){
-            return "$amount " + Symbols[currencyCode]
+            return "$balance " + Symbols[currencyCode]
         }
-        return "$amount $currencyCode"
+        return "$balance $currencyCode"
     }
 
     companion object {
 
-        val Symbols: HashMap<String, String> = hashMapOf("RUB" to "₽", "CZK" to "Kč", "USD" to "$", "EUR" to "€", "THB" to "฿")
+        val Symbols: HashMap<String, String> = hashMapOf(
+                "CZK" to "Kč",
+                "EUR" to "€",
+                "GNF" to "FG",
+                "HRK" to "Kn",
+                "HUF" to "ƒ",
+                "LKR" to "₨",
+                "MNT" to "₮",
+                "PLN" to "zł",
+                "RUB" to "₽",
+                "THB" to "฿",
+                "TRY" to "₺",
+                "USD" to "$",
+                "VND" to "₫")
 
         fun newInstance(balance: Double, currencyCode: String) : MoneyCurrency {
             val currency = MoneyCurrency()
-            currency.amount = balance
+            currency.balance = balance
             currency.currencyCode = currencyCode
             return currency
         }
